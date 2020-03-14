876125d0ece07e882952f1087486e3a351c96b0b
carlos <radix_10339@hotmail.com>
2020-03-13 23:16:44 -0600

    unrar code

---
+++ U1_A2_Genericos/U1_A2_Genericos/Basketbolista.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U1_A2_Genericos
+{
+    class Basketbolista : Deportista
+    {
+        public byte PuntosAnotados { get; set; }
+        public short NoRebores { get; set; }
+        public short NoAsistencias { get; set; }
+
+
+
+        public Basketbolista(string noControl, string nom, string apepa, string apema, byte ed, string sex, double est, float pes, byte panot, short rebotes, short asistencias)
+        {
+            No_Control = noControl;
+            Nombre = nom;
+            ApePat = apepa;
+            ApeMat = apema;
+            Edad = ed;
+            Sexo = sex;
+            Estatura = est;
+            Peso = pes;
+            PuntosAnotados = panot;
+            NoRebores = rebotes;
+            NoAsistencias = asistencias;
+
+        }
+
+        public override string ToString()
+        {
+            return "Basket" + "/" + No_Control + "/" + Nombre + "/" + ApePat + "/" + ApeMat + "/" + Edad + "/" + Sexo + "/" + Estatura + "/" + Peso + "/" + PuntosAnotados + "/" + NoRebores + "/" + NoAsistencias;
+            //return this.ToString();
+        }
+    }
+}
---
+++ U1_A2_Genericos/U1_A2_Genericos/Predicados.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U1_A2_Genericos
+{
+    class Predicados
+    {
+        internal static bool ValidarSexo(Deportista deportista)
+        {
+            if(deportista.Sexo == "Femenino")
+            {
+                return true;
+                
+            }
+            return false;
+        }
+
+
+        internal static bool ValidarEquipo(Equipo<Deportista> deportista, Deportista dep)
+        {
+
+            return deportista.Tipo == dep.Sexo;
+
+
+        }
+
+
+        //
+        //if ((deportista.Tipo=="Varonil") && (dep.Sexo=="M"))
+        //{
+        //    return true;
+        //}
+        //return false;
+        //public static bool NoMaxIntegrantes(Equipo<Deportista> e)
+        //{
+        //    return e.Integrantes.Count < e.NoIntegrantes;
+        //}
+    }
+}
